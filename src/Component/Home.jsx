ea7b5384ba77131c2ab17e304d2b03d57ae6b07f
nahian255 <nahimn253@gmail.com>
2023-05-03 10:23:36 +0600

    main part almost done

--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import logo from '../assets/bannerimg.jpg'
 import ChefCard from './aboutChef/ChefCard';
+import { Carousel } from 'flowbite-react';
 
 const Home = () => {
     const [chefs, setChefs] = useState([])
@@ -16,30 +17,63 @@ const Home = () => {
 
     return (
         <div>
-            <h1>home</h1>
+            {/* banner part.......... */}
             <div
                 // style={{ backgroundImage: `url(${logo})` }}
-                className='lg:flex '>
-                <div className=' h-2/4 lg:h-full lg:w-6/12 text-center'>
-                    <h1 className='text-xl'>this is the text section..</h1>
-                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi porro odit itaque possimus dolor autem, fuga vero numquam iure est cupiditate. Alias illum, inventore dolore eius quos consequatur iure quaerat.</p>
-                    <p>Another paragrare section that make it more pimpleee</p>
+                className='lg:flex my-4  bg-rose-50 rounded-lg'>
+                <div className=' h-2/4 lg:h-full lg:w-6/12 text-center p-2 '>
+                    <h1 className='text-4xl italic text-blue-600 my-6 lg:my-10'>Meet the Top Chef in Eupors</h1>
 
+                    <p className='my-2 lg:my-4 mx-4 text-left italic'>
+                        {/* <span className='text-xl text-blue-600'></span> */}
+                        Top Chef is a cooking show that uses a progressive elimination format. The beginning of each season starts with twelve to nineteen professional chefs selected through auditions. The chefs are brought to the season's host city or state, which typically inspires themes throughout the season.
+                    </p>
+                    <p className='text-left p-4'> You want to know about more about Chef,then scroll down.</p>
                 </div>
                 <div className='bg-red-300 h-2/4 lg:h-full  lg:w-6/12'>
-                    <h1>this is img section a huge cute img on that</h1>
+
                     <img src={logo} alt="" />
                 </div>
             </div>
-            <section className='bg-yellow-100 my-16'>
-                <h1>Chef Section</h1>
-                <div className='flex justify-evenly flex-wrap gap-8'>
+            {/* Card part............ */}
+            <section className=' my-16'>
+                <h1 className='text-2xl lg:text-4xl my-4 italic' > Top 6 Chef in this regiouns.</h1>
+                <div className='flex justify-evenly flex-wrap my-4 gap-8'>
                     {
                         chefs.map(chef => <ChefCard
                             key={chef.id}
                             chef={chef}
                         ></ChefCard>)
                     }
+                </div>
+            </section>
+            {/* Another two sectionnn */}
+            <section>
+                <h1 className='text-2xl lg:text-4xl my-4 italic' > Meet another Famous Chef in this twon.</h1>
+
+                <div className="h-56 sm:h-64 xl:h-screen 2xl:h-96">
+                    <Carousel>
+                        <img
+                            src={logo}
+                            alt="..."
+                        />
+                        <img
+                            src="https://flowbite.com/docs/images/carousel/carousel-2.svg"
+                            alt="..."
+                        />
+                        <img
+                            src="https://flowbite.com/docs/images/carousel/carousel-3.svg"
+                            alt="..."
+                        />
+                        <img
+                            src="https://flowbite.com/docs/images/carousel/carousel-4.svg"
+                            alt="..."
+                        />
+                        <img
+                            src="https://flowbite.com/docs/images/carousel/carousel-5.svg"
+                            alt="..."
+                        />
+                    </Carousel>
                 </div>
             </section>
         </div>
--- src/Component/aboutChef/ChefRecipes.jsx
+++ src/Component/aboutChef/ChefRecipes.jsx
@@ -1,30 +1,41 @@
-import { Accordion } from 'flowbite-react';
+import { Accordion, Button, Rating, Toast } from 'flowbite-react';
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const ChefRecipes = () => {
     const data = useLoaderData()
     console.log(data);
-    const { name, id, img, Likes, recipes, experience, recipe1, recipe1Ingredients, recipe1Method, recipe2, recipe2Ingredients, recipe2Method, recipe3, recipe3Ingredients, recipe3Method } = data;
+    const { name, id, img, body, Likes, recipes, experience, recipe1, recipe1Ingredients, recipe1Method, recipe2, recipe2Ingredients, recipe2Method, recipe3, recipe3Ingredients, recipe3Method } = data;
 
+    const handelAddFav = (event) => {
+        event.currentTarget.disabled = true;
+        console.log('clicked');
+        toast("Added your Favorite")
+
+    };
     return (
         <>
+            {/* chef information */}
             <div
                 // style={{ backgroundImage: `url(${logo})` }}
-                className='lg:flex my-4 p-4 '>
-                <div className=' h-2/4 lg:h-full lg:w-6/12 text-center '>
-                    <h1 className='text-4xl '>{name}</h1>
-                    <p>someting that happen </p>
-                    <p>that is about chef bio</p>
-
+                className='lg:flex my-4  bg-rose-100 rounded-lg'>
+                <div className=' h-2/4 lg:h-full lg:w-6/12 text-center p-2 '>
+                    <h1 className='text-6xl italic text-blue-600 my-6 lg:my-10'>{name}</h1>
+                    <p className='italic text-red-700'>Top famous Chef in Euroupe. </p>
+                    <p className='my-2 lg:my-4 mx-4 text-left'><span className='text-xl text-blue-600'>{name}</span>{body}</p>
+                    <p className='text-left p-4'>Number of recipes : 15 item</p>
+                    <p className='text-left p-4'>Likes: {Likes}</p>
                 </div>
                 <div className='bg-red-300 h-2/4 lg:h-full  lg:w-6/12'>
 
                     <img src={img} alt="" />
                 </div>
             </div>
+            {/* recipies item........... */}
             <div className='my-8'>
-                <h1 className='text-4xl text-red-500'>Time to show some Famous recepi of <span className='text-blue-500'>{name}</span></h1>
+                <h1 className=' my-2 lg:my-8 text-2xl lg:text-5xl  text-black italic'>Time to show some Famous recepi of  <span className='text-blue-500'>{name}</span></h1>
                 <div className='p-4'>
                     <Accordion>
                         <Accordion.Panel>
@@ -42,6 +53,22 @@ const ChefRecipes = () => {
                                 >
                                     <span className='text-lg text-blue-700'> Cooking Method</span> : {recipe1Method}.
                                 </p>
+                                <Rating>
+                                    <Rating.Star />
+                                    <Rating.Star />
+                                    <Rating.Star />
+                                    <Rating.Star />
+                                    <Rating.Star filled={false} />
+                                </Rating>
+                                <div className='my-2'>
+                                    <Button
+                                        onClick={handelAddFav}
+                                        color="purple">
+                                        Add to Favorite
+                                    </Button>
+                                    <ToastContainer />
+                                </div>
+
 
                             </Accordion.Content>
                         </Accordion.Panel>
@@ -60,6 +87,18 @@ const ChefRecipes = () => {
                                 >
                                     <span className='text-lg text-blue-700'> Cooking Method</span> : {recipe2Method}
                                 </p>
+                                <Rating>
+                                    <Rating.Star />
+                                    <Rating.Star />
+                                    <Rating.Star />
+                                    <Rating.Star />
+                                    <Rating.Star filled={false} />
+                                </Rating>
+                                <div className='my-2'>
+                                    <Button color="purple">
+                                        Add to Favorite
+                                    </Button>
+                                </div>
                             </Accordion.Content>
                         </Accordion.Panel>
                         <Accordion.Panel>
@@ -77,12 +116,26 @@ const ChefRecipes = () => {
                                 >
                                     <span className='text-lg text-blue-700'> Cooking Method</span> : {recipe3Method}
                                 </p>
+                                <Rating>
+                                    <Rating.Star />
+                                    <Rating.Star />
+                                    <Rating.Star />
+                                    <Rating.Star />
+                                    <Rating.Star filled={false} />
+                                </Rating>
+                                <div className='my-2'>
+                                    <Button
+
+                                        color="purple">
+                                        Add to Favorite
+                                    </Button>
+                                </div>
 
                             </Accordion.Content>
                         </Accordion.Panel>
                     </Accordion>
                 </div>
-            </div>
+            </div >
         </>
     );
 };
